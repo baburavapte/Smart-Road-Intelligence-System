dd52c14ca80e9608249c83cee5b8e3eb19898699
Artist <105969821+baburavapte@users.noreply.github.com>
2026-08-11 18:13:35 +0530

    Fix Railway Flask port

--- inference-service/app.py
+++ inference-service/app.py
@@ -158,4 +158,5 @@ def get_upload_image(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
